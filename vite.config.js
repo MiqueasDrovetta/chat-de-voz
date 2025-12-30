1d26504a741850073288833c875f34be58c07f8e
miqueas.drovetta <miqueas.drovetta@itscordoba.edu.ar>
2025-12-30 20:29:50 +0000

    Feat: Configuración de Vite condicional para desarrollo y producción

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // Establece la base condicionalmente
+    // Si el comando es 'serve' (desarrollo), la base es '/'
+    // Si el comando es 'build' (producción), la base es '/chat-de-voz/'
+    base: command === 'serve' ? '/' : '/chat-de-voz/',
+  }
 })
